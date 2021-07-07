3244f1b3d3bdf078912e4d29f1ce0757b45e9df6
mm4130 <mm41.ir@gmail.com>
2021-07-07 16:13:34 +0430

    Update config.py

--- bot/webgram/config.py
+++ bot/webgram/config.py
@@ -3,14 +3,14 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 2544613
 
-        API_HASH = "......"
+        API_HASH = "0f412888935b3f1eaad6de9d3f4e425c"
         
-        BOT_TOKEN = "......."
+        BOT_TOKEN = "1871931544:AAHzxYmGPF3_ZPxuG7bzfc-B-Kuzl5ao4IU"
                 
-        channel = 'UserlandApp'
+        channel = 'mddownl'
         
-        ROOT_URI = f"http://dl1.dlgram.ml"
+        ROOT_URI = f"https://mydlr.herokuapp.com/"
 
         ENCODING = "utf8"
