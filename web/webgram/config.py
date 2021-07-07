408fc938deb0ac03a6e3c98a2c725bd9a676d695
mm4130 <mm41.ir@gmail.com>
2021-07-07 16:30:42 +0430

    Update config.py

--- web/webgram/config.py
+++ web/webgram/config.py
@@ -3,17 +3,17 @@ import os
 class Config:
 
     class config:
-        APP_ID = 50807
+        APP_ID = 2544613
 
-        API_HASH = "......"
+        API_HASH = "0f412888935b3f1eaad6de9d3f4e425c"
         
-        BOT_TOKEN = "...."
+        BOT_TOKEN = "1871931544:AAHzxYmGPF3_ZPxuG7bzfc-B-Kuzl5ao4IU"
         
         HOST = "127.0.0.1"
 
         PORT = os.getenv('PORT')
 
-        ROOT_URI = f"http://dlstarus.dlgram.ml"
+        ROOT_URI = f"https://mydlr.herokuapp.com/"
 
 
         ENCODING = "utf8"
